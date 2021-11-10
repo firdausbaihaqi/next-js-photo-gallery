be9d77edb09ca13c6b02e59cbb4c63630193eed7
Muhammad Ihya Firdaus Baihaqi <43091431+firdausbaihaqi@users.noreply.github.com>
2021-11-10 18:26:28 +0700

    fix get img size from url

--- components/UI/Img.jsx
+++ components/UI/Img.jsx
@@ -6,27 +6,30 @@ function Img({ url }) {
   const [imgWidth, setImgWidth] = useState(0);
   const [imgHeight, setImgHeight] = useState(0);
 
-  useEffect(() => {
-    // make new image instance to get it's dimension
-    const img = new Image();
-    img.src = url;
+  // make new image instance to get it's dimension
+  const img = new Image();
+  img.src = url;
 
+  useEffect(() => {
     setImgWidth(img.width);
     setImgHeight(img.height);
-  }, [url]);
+    console.log(img.width, img.height);
+    console.log(imgWidth, imgHeight);
+  }, [img.width, img.height]); //img dimension always start at 0 because it's not fully loaded
 
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ delay: 1 }}
-      className="block w-full max-h-[600px] my-2 rounded cursor-pointer relative"
+      className="block w-full max-h-[600px] my-2 relative"
     >
       <NextImage
         src={url}
         width={imgWidth}
         height={imgHeight}
         objectFit="cover"
+        className="rounded cursor-pointer"
       />
     </motion.div>
   );
